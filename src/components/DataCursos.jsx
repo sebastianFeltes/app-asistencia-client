693dcf7fea2270716b65ea0158c361991225d94e
cflpc001 <cflpc012023@gmail.com>
2023-08-14 20:32:08 -0300

    mapeo data cursos

--- src/components/DataCursos.jsx
+++ src/components/DataCursos.jsx
@@ -1,16 +1,30 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getDataCursos } from "../services/DataCursos.services";
 
 function DataCursos() {
+  const { data, isLoading, error } = useQuery(["getCursos"], getDataCursos);
+  !data ? false : console.log(data);
+
   return (
     <div className="hero min-h-screen bg-slate-50 text-black">
       <div className="hero-content text-center">
         <div className="overflow-x-auto">
           <div className=" flex justify-between">
-            
-          <Link to={"/"}> <button className="btn btn-active btn-neutral ">Nuevo Curso</button></Link>
-          <Link to={"/"}> <button className="btn btn-active btn-neutral">Historial Cursos</button></Link>
+            <Link to={"/"}>
+              {" "}
+              <button className="btn btn-active btn-neutral ">
+                Nuevo Curso
+              </button>
+            </Link>
+            <Link to={"/"}>
+              {" "}
+              <button className="btn btn-active btn-neutral">
+                Historial Cursos
+              </button>
+            </Link>
           </div>
-        
+
           <table className="table  text-center">
             {/* head  */}
             <thead className=" text-black  font-normal">
@@ -26,78 +40,106 @@ function DataCursos() {
             </thead>
             {/* SE AUTO RELLENA CON LOS DATOS DE LOS CURSOS CARGADOS */}
             <tbody>
-              {/* row 1 */}
+              {!data
+                ? false
+                : data.map((e) => (
+                    <tr key={e}>
+                      <td>{e.nombre}</td>
+                      <td>{e.profesor}</td>
+                      <td>{e.dias}</td>
+                      <td>{e.horarios}</td>
+                      <td>{e.cantAlumnos}</td>
+                      <td>
+                        <input type="checkbox" className="toggle" />
+                      </td>
+                      <td>
+                        <button className="btn btn-active">edit</button>
+                      </td>
+                    </tr>
+                  ))}
+              {/*               <tr>
+                <td>ejemplo</td>
+                <td>ejemplo</td>
+                <td>ejemplo</td>
+                <td>ejemplo</td>
+                <td>ejemplo</td>
+                <td>
+                  <input type="checkbox" className="toggle" />
+                </td>
+                <td>
+                  <button className="btn btn-active">edit</button>
+                </td>
+              </tr>
+
               <tr>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
-                <td><input type="checkbox" className="toggle"  /></td>
+                <td>
+                  <input type="checkbox" className="toggle" />
+                </td>
                 <td>
                   <button className="btn btn-active">edit</button>
                 </td>
               </tr>
-              {/* row 2 */}
+
               <tr>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
-                <td><input type="checkbox" className="toggle"  /></td>
+                <td>
+                  <input type="checkbox" className="toggle" />
+                </td>
                 <td>
                   <button className="btn btn-active">edit</button>
                 </td>
               </tr>
-              {/* row 3 */}
+
               <tr>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
-                <td><input type="checkbox" className="toggle"  /></td>
+                <td>
+                  <input type="checkbox" className="toggle" />
+                </td>
                 <td>
                   <button className="btn btn-active">edit</button>
                 </td>
               </tr>
-              {/* row 4 */}
+
               <tr>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
-                <td><input type="checkbox" className="toggle"  /></td>
+                <td>
+                  <input type="checkbox" className="toggle" />
+                </td>
                 <td>
                   <button className="btn btn-active">edit</button>
                 </td>
               </tr>
-              {/* row 5 */}
+
               <tr>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
                 <td>ejemplo</td>
-                <td><input type="checkbox" className="toggle" /></td>
+                <td>
+                  <input type="checkbox" className="toggle" />
+                </td>
                 <td>
                   <button className="btn btn-active">edit</button>
                 </td>
-              </tr>
-              {/* row 6 */}
-              <tr>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td><input type="checkbox" className="toggle" /></td>
-                <td>
-                  <button className="btn btn-active">edit</button>
-                </td>
-              </tr>
+              </tr> */}
             </tbody>
           </table>
         </div>
