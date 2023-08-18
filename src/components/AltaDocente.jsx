9d97fc5b8c5f2ea84e9ec26a5f1fe73ab6c764c0
Maxi20VZ <velazquezmaximiliano039@gmail.com>
2023-08-18 17:05:26 -0300

    enmaquetado

--- src/components/AltaDocente.jsx
+++ src/components/AltaDocente.jsx
@@ -1,105 +1,144 @@
 function AltaDocente() {
-    return (
-        <>
-            <div className="hero min-h-screen bg-white">
-                <div className="hero-content text-center w-full">
-                    <div className="w-full">
-                        <h1 className="text-5xl text-black font-bold m-2">Alta Docente</h1>
+  return (
+    <>
+      <div className="hero min-h-screen bg-gray-100">
+        <div className="hero-content text-center w-full border border-black">
+          <div className="w-full">
+            <h2 className="text-3xl text-black font-bold mb-8">Alta Docente</h2>
 
-                        <div className="flex flex-row columns-3xl">
-                            <div className="form-control w-full max-w-full  border border-black p-4 m-5">
-                                <label className="label">
-                                    <span className="label-text  text-black">Documento</span>
-                                </label>
-
-                                <input type="text" placeholder="Dni" className="input input-bordered  bg-white w-full max-w-3xl" />
-                                <span className="label-text  text-black">Tipo De Doc</span>
-                                <label className="label cursor-pointer">
-                                    <span className="label-text  text-black">DU</span>
-                                    <input type="radio" name="radio-10" className="radio checked:bg-red-00" checked />
-                                </label>
-                                <label className="label cursor-pointer">
-                                    <span className="label-text text-black">LC</span>
-                                    <input type="radio" name="radio-10" className="radio checked:bg-blue-00" checked />
-                                </label>                            <label className="label cursor-pointer">
-                                    <span className="label-text  text-black">LE</span>
-                                    <input type="radio" name="radio-10" className="radio checked:bg-blue-00" checked />
-                                </label>
-
-
-                            </div>
-
-                            <div className="form-control w-full max-w-full border border-black p-4 m-5">
-                                <label className="label">
-                                    <span className="label-text  text-black">Nombre Docente</span>
-                                </label>
-                                <input type="text" placeholder="Nombre Del Profesor" className="input input-bordered  bg-white w-full max-w-3xl" />
-
-                                <label className="label">
-                                    <span className="label-text  text-black">Apellido Docente</span>
-                                </label>
-                                <input type="text" placeholder="Nombre Del Curso" className="input input-bordered  bg-white w-full max-w-3xl" />
-                            </div>
-                            <div className="form-control w-full max-w-full  border border-black p-4 m-5">
-                                <label className="label">
-                                    <span className="label-text text-black">Direccion</span>
-                                </label>
-                                <input type="text" placeholder="-----" className="input input-bordered  bg-white w-full max-w-3xl" />
-
-                                <label className="label">
-                                    <span className="label-text  text-black">Localidad</span>
-                                </label>
-                                <select className="select  bg-white  text-black w-full max-w-full">
-                                    <option disabled selected>What is city from?</option>
-                                    <option>City Of Berisso</option>
-                                    <option>City Of La Plata</option>
-                                    <option>City Of Ensenada</option>
-
-                                </select>
-                            </div>
-
-                        </div>
-                        <div className="flex flex-row">
-                            <div className="form-control w-full max-w-full border border-black p-4 m-5 ">
-                                <label className="label">
-                                    <span className="label-text  text-black">Telefono</span>
-                                </label>
-                                <input type="text" placeholder="Numero" className="input input-bordered bg-white w-full max-w-3xl" />
-
-                                <label className="label">
-                                    <span className="label-text  text-black">Email</span>
-                                </label>
-                                <input type="text" placeholder="Nombre Del Curso" className="input input-bordered  bg-white w-full max-w-3xl" />
-                            </div>
-                            <div className="form-control w-full max-w-full  border border-black p-4 m-5">
-                                <label className="label">
-                                    <span className="label-text  text-black">Tel Extra</span>
-                                </label>
-                                <input type="text" placeholder="XXXXX" className="input input-bordered  bg-white w-full max-w-3xl" />
-
-                                <label className="label">
-                                    <span className="label-text  text-black">Rol</span>
-                                </label>
-                                <input type="text" placeholder="Type here" className="input input-bordered  bg-white w-full max-w-3xl" />
-
-                            </div>
-
-
-
-
-                        </div>
-
-                        <div className="flex 
-                        ">
-                            <form> <button className="btn btn-outline">Default</button></form>
-                            <form> <button className="btn btn-outline">Default</button></form>
-                        </div>
-                        
-                    </div>
+            <form className="grid grid-cols-2 gap-4 ">
+              <div className="flex flex-col m-2">
+                <label className="label">
+                  <span className="label-text  text-black">Documento</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs bg-white border-black"
+                />
+                <label className="label">
+                  <span className="label-text  text-black">Tipo De Doc</span>
+                </label>
+                <div className="from-control flex flex-row m-2">
+                  <label className="label cursor-pointer">
+                    <span className="label-text  text-black">DU</span>
+                    <input
+                      type="radio"
+                      name="radio-10"
+                      className="radio checked:bg-red-00"
+                      checked
+                    />
+                  </label>
+                  <label className="label  cursor-pointer">
+                    <span className="label-text text-black">LC</span>
+                    <input
+                      type="radio"
+                      name="radio-10"
+                      className="radio checked:bg-blue-00"
+                      checked
+                    />
+                  </label>{" "}
+                  <label className="label cursor-pointer">
+                    <span className="label-text  text-black">LE</span>
+                    <input
+                      type="radio"
+                      name="radio-10"
+                      className="radio checked:bg-blue-00"
+                      checked
+                    />
+                  </label>
                 </div>
-            </div>
 
-        </>
-    )
+                <label className="label">
+                  <span className="label-text  text-black">Nombre Docente</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs bg-white border-black"
+                />
+
+                <label className="label">
+                  <span className="label-text  text-black">
+                    Apellido Docente
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs bg-white border-black"
+                />
+
+                <label className="label">
+                  <span className="label-text text-black">Direccion</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs bg-white border-black"
+                />
+
+                <label className="label">
+                  <span className="label-text text-black">Localidad</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs bg-white border-black"
+                />
+              </div>
+
+              <div className="grid grid-cols-2 gap-4 ">
+                <div className=" ">
+                  <label className="label">
+                    <span className="label-text  text-black">Tel Extra</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs bg-white border-black"
+                  />
+
+                  <label className="label">
+                    <span className="label-text  text-black">Rol</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs bg-white border-black"
+                  />
+                  <label className="label">
+                    <span className="label-text  text-black">Telefono</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs bg-white border-black"
+                  />
+
+                  <label className="label">
+                    <span className="label-text  text-black">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs bg-white border-black"
+                  />
+                </div>
+              </div>
+              <div className="content-center m-2">
+                <button className="btn bg-blue-900 text-black">Cancelar</button>
+              </div>
+              <div className="content-center m-2">
+                <button type="submit" className="btn bg-blue-900 text-black">
+                  Aceptar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default AltaDocente
+export default AltaDocente;
--- src/services/AltaDocente.services.js
+++ src/services/AltaDocente.services.js
@@ -1,3 +1,39 @@
-export function tryAltaDocente(usuario){
-    alert( `el usuario ${usuario} no tiene permiso... `)
+export async function tryAltaDocente(
+  dni,
+  nombre,
+  apellido,
+  direccion,
+  telefono,
+  localidad,
+  email,
+  telExt,
+  rol
+) {
+  const data = {
+    dni:dni,
+    nombre:nombre,
+    apellido:apellido,
+    direccion:direccion,
+    telefono:telefono,
+    localidad:localidad,
+    email:email,
+    telExt:telExt,
+    rol:rol,
+  };
+  const response = await fetch("http://192.168.33.31:8080/alta-docente");
+  ({
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  const res = await response.json();
+  return res;
 }
