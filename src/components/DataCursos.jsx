b326cceee1e683ac1049f877bf65b7b4924bda9d
cflpc001 <cflpc012023@gmail.com>
2023-08-25 20:43:34 -0300

    modal

--- src/components/DataCursos.jsx
+++ src/components/DataCursos.jsx
@@ -7,19 +7,20 @@ function DataCursos() {
   !data ? false : console.log(data);
 
   return (
-    <div className="hero min-h-screen bg-slate-50 text-black">
-      <div className="hero-content text-center">
+    <div className="hero min-h-screen bg-slate-50 text-black tabla-data-cursos">
+      <div className="hero-content text-center p-0 w-full">
         <div className="overflow-x-auto">
           <div className=" flex justify-between">
             <Link to={"/"}>
               {" "}
-              <button className="btn btn-active btn-neutral ">
+              <button className="btn bg-blue-600 text-white hover:bg-blue-300  hover:text-black ">
                 Nuevo Curso
               </button>
             </Link>
+
             <Link to={"/"}>
               {" "}
-              <button className="btn btn-active btn-neutral">
+              <button className="btn bg-blue-600 text-white hover:bg-blue-300  hover:text-black">
                 Historial Cursos
               </button>
             </Link>
@@ -27,7 +28,7 @@ function DataCursos() {
 
           <table className="table  text-center">
             {/* head  */}
-            <thead className=" text-black  font-normal">
+            <thead className=" text-black  font-semibold">
               <tr>
                 <th>NOMBRE DEL CURSO</th>
                 <th>PROFESOR</th>
@@ -43,9 +44,11 @@ function DataCursos() {
               {!data
                 ? false
                 : data.map((e) => (
-                    <tr key={e}>
-                      <td>{e.nombre}</td>
-                      <td>{e.profesor}</td>
+                    <tr className="hover:bg-slate-200" key={e}>
+                      <td className="hover:italic">{e.nombre.toUpperCase()}</td>
+                      <td className="hover:italic">
+                        {e.profesor.toUpperCase()}
+                      </td>
                       <td>{e.dias}</td>
                       <td>{e.horarios}</td>
                       <td>{e.cantAlumnos}</td>
@@ -53,95 +56,65 @@ function DataCursos() {
                         <input type="checkbox" className="toggle" />
                       </td>
                       <td>
-                        <button className="btn btn-active">edit</button>
+                        <button className="btn  bg-blue-600 text-white hover:bg-blue-300  hover:text-black">
+                          edit
+                        </button>
                       </td>
                     </tr>
                   ))}
-              {/*               <tr>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>
-                  <input type="checkbox" className="toggle" />
-                </td>
-                <td>
-                  <button className="btn btn-active">edit</button>
-                </td>
-              </tr>
-
-              <tr>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>
-                  <input type="checkbox" className="toggle" />
-                </td>
-                <td>
-                  <button className="btn btn-active">edit</button>
-                </td>
-              </tr>
-
-              <tr>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>
-                  <input type="checkbox" className="toggle" />
-                </td>
-                <td>
-                  <button className="btn btn-active">edit</button>
-                </td>
-              </tr>
-
-              <tr>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>
-                  <input type="checkbox" className="toggle" />
-                </td>
-                <td>
-                  <button className="btn btn-active">edit</button>
-                </td>
-              </tr>
-
-              <tr>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>
-                  <input type="checkbox" className="toggle" />
-                </td>
-                <td>
-                  <button className="btn btn-active">edit</button>
-                </td>
-              </tr>
-
-              <tr>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>ejemplo</td>
-                <td>
-                  <input type="checkbox" className="toggle" />
-                </td>
-                <td>
-                  <button className="btn btn-active">edit</button>
-                </td>
-              </tr> */}
             </tbody>
           </table>
+
+          <div
+            id="modal form-control"
+            className="fixed w-full h-full m-0 p-0 top-0 left-0  flex flex-row justify-center bg-gray-400 border border-cyan-400"
+          >
+            <div className="flex flex-col place-items-center justify-center border w-full border-blue-400">
+              
+              <span className="label-text  text-black">Nombre Del Curso</span>
+              <input
+                type="text"
+              
+                className="input input-bordered w-full  max-w-lg bg-white text-black border border-blue-400"
+              />
+              <span className="label-text  text-black">Profesor</span>
+              <input
+                type="text"
+               
+                className="input input-bordered w-full max-w-lg bg-white text-black border border-blue-400"
+              />
+              <span className="label-text   text-black">Dias</span>
+              <input
+                type="text"
+              
+                className="input input-bordered w-full max-w-lg bg-white text-black border border-blue-400"
+              />
+              <span className="label-text   text-black">Horario</span>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-lg bg-white text-black border border-blue-400"
+              />
+              <span className="label-text   text-black">Cantidad De Alumnos</span>
+              <input
+              
+              
+                type="number"
+                className="input input-bordered w-full max-w-lg bg-white text-black border border-blue-400"
+              />
+              
+              <div className="flex justify-center">
+
+              <button className="btn   max-w-xs bg-[#0184F5] text-white m-2">
+                Aceptar
+              </button>
+              <button className="btn  max-w-xs bg-[#0184F5] text-white m-2">
+                Cancelar
+              </button>
+              </div>
+            </div>
+
+           
+          </div>
         </div>
       </div>
     </div>
