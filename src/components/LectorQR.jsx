b302947261fecaf5cf84f4491cbae4eed4239458
JikanSui <jikansui16@gmail.com>
2023-09-27 11:10:52 -0300

    pantalla de gestion qr, servicios gestion qr

--- src/components/LectorQR.jsx
+++ src/components/LectorQR.jsx
@@ -1,57 +1,59 @@
 import { useState } from "react";
+import { buscarAlumnoPorID } from "../services/GestionQR.services";
 
 export default function LectorQR() {
-    const [mostrarData, setMostrarData] = useState(false);
-    const sendInfo = (e) => {
-        e.preventDefault();
-        let currentValue = e.target.value;
-        if (currentValue.includes(".")) {
-            let findedNum = parseInt(e.target.value.split(".")[0])
-            //TODO: enviar findedNum al servidor para traer los datos del alumno
+	const [mostrarData, setMostrarData] = useState(false);
+	const [alumno, setAlumno] = useState(false);
+	const sendInfo = async (e) => {
+		e.preventDefault();
+		let currentValue = e.target.value;
+		if (currentValue.includes(".")) {
+			let findedNum = parseInt(e.target.value.split(".")[0]);
 
-            console.log(findedNum)
-            setMostrarData(!mostrarData)
-        }
+			//TODO: enviar findedNum al servidor para traer los datos del alumno
+			const res = await buscarAlumnoPorID(findedNum);
+			setAlumno(res);
+			console.log(findedNum);
+			setMostrarData(!mostrarData);
+		}
+	};
 
-    }
-
-    const alumno = {
-        id_alumno: 102,
-        nro_legajo: 1023,
-        nombre: "Sebastian",
-        apellido: "Feltes",
-        tipo_dni: "DU",
-        nro_dni: 35954987
-    }
-    return (
-        <div className="hero min-h-screen bg-white ">
-            <div className="hero-content w-full text-center">
-                <div className="w-full flex flex-col ">
-                    <h1 className="text-5xl m-8 font-bold">{mostrarData?"Datos del alumno":"Escanee su código.."}</h1>
-                    <input onChange={e => sendInfo(e)} id="nombreAlumno" type="password" placeholder="Código" className={!mostrarData?"rounded-full input input-bordered input-info max-w-xs text-center ml-auto mr-auto bg-white border-black":"hidden"} />
-                    <div className={mostrarData?"block":"hidden"}>
-
-                        <div className="w-1/2 h-full ml-auto mr-auto bg-white flex flex-col " >
-                            <span className=" w-1/2 p-3 font-normal my-4  ml-auto mr-auto  border-b border-blue-600 rounded-lg">
-                                {alumno.apellido}
-                            </span><span className=" w-1/2 p-3 my-4 font-normal  ml-auto mr-auto   border-b border-blue-600 rounded-lg">
-                                {alumno.nombre}
-                            </span>
-                            <span className=" w-1/2 p-3 font-normal my-4  ml-auto mr-auto  border-b border-blue-600 rounded-lg">
-                                {alumno.nro_dni}
-                            </span>
-                            <span className=" w-1/2 p-3 font-normal my-4  ml-auto mr-auto  border-b border-blue-600 rounded-lg">
-                                {new Date().toLocaleString('es-ES')}
-                            </span>
-                            
-
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    );
-
-
+	return (
+		<div className="hero min-h-screen bg-white ">
+			<div className="hero-content w-full text-center">
+				<div className="w-full flex flex-col ">
+					<h1 className="text-5xl m-8 font-bold">
+						{mostrarData ? "Datos del alumno" : "Escanee su código.."}
+					</h1>
+					<input
+						onChange={(e) => sendInfo(e)}
+						id="nombreAlumno"
+						type="password"
+						placeholder="Código"
+						className={
+							!mostrarData
+								? "rounded-full input input-bordered input-info max-w-xs text-center ml-auto mr-auto bg-white border-black"
+								: "hidden"
+						}
+					/>
+					<div className={mostrarData ? "block" : "hidden"}>
+						<div className="w-1/2 h-full ml-auto mr-auto bg-white flex flex-col ">
+							<span className=" w-1/2 p-3 font-normal my-4  ml-auto mr-auto  border-b border-blue-600 rounded-lg">
+								{alumno ? alumno.apellido : false}
+							</span>
+							<span className=" w-1/2 p-3 my-4 font-normal  ml-auto mr-auto   border-b border-blue-600 rounded-lg">
+								{alumno ? alumno.nombre : false}
+							</span>
+							<span className=" w-1/2 p-3 font-normal my-4  ml-auto mr-auto  border-b border-blue-600 rounded-lg">
+								{alumno ? alumno.nro_dni : false}
+							</span>
+							<span className=" w-1/2 p-3 font-normal my-4  ml-auto mr-auto  border-b border-blue-600 rounded-lg">
+								{new Date().toLocaleString("es-ES")}
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
---
+++ src/services/GestionQR.services.js
@@ -0,0 +1,8 @@
+export async function buscarAlumnoPorID(id) {
+	const url = "http://localhost:8080/alumno";
+	// Default options are marked with *
+	const response = await fetch(`${url}/${id}`);
+	const res = response.json(); // parses JSON response into native JavaScript objects
+	console.log(res);
+	return res;
+}
