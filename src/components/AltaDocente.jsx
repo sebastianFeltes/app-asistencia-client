87727b89549253a9b21eee71d800d87eef31afb7
cfl014 <cfl.404.bsso@gmail.com>
2023-09-27 14:07:58 -0300

    fix

--- src/components/AltaDocente.jsx
+++ src/components/AltaDocente.jsx
@@ -11,32 +11,40 @@ export default function AltaDocente() {
     const nombreDoc = e.target.nombreDoc.value;
     const apellidoDoc = e.target.apellidoDoc.value;
     const contra = e.target.contra.value;
+    const contra2 = e.target.contra2.value;
     const direc = e.target.direc.value;
     const local = e.target.local.value;
-    const tele = e.target.telex.value;
+    const codi1 = e.target.codi1.value
+    const tele = e.target.tele.value;
+    const codi2 = e.target.codi2.value;
+    const telex = e.target.telex.value;
     const rol = e.target.rol.value;
-    const telex = e.target.tele.value;
     const correo = e.target.correo.value;
 
     const data = {
-      tipo: tipoDoc,
-      dni: dni,
+      tipo_dni: tipoDoc,
+      nro_dni: parseInt(dni),
       nombre: nombreDoc,
       apellido: apellidoDoc,
-      contrasena: contra,
+      password: contra,
       direccion: direc,
       localidad: local,
-      telefono: tele,
-      rol: rol,
-      telExt: telex,
-      email: correo
-
+      car_telefono: parseInt(codi1),
+      telefono: parseInt(tele),
+      car_tel_extra: parseInt(codi2),
+      telefono_extra: parseInt(telex),
+      id_rol: rol == "ADMIN" ? 2 : 1,
+      email: correo,
+      activo: true
     };
 
-
-    const res = await tryAltaDocente(data);
-
-    return res;
+    console.log(data)
+    if (contra === contra2) {
+      const res = await tryAltaDocente(data);
+      return console.log(res);
+    } else {
+      alert("las contraseñas deben ser iguales")
+    }
 
 
 
@@ -51,11 +59,14 @@ export default function AltaDocente() {
     <>
       <div className="hero min-h-screen bg-white">
         <div className="hero-content text-center  border-4 border-black rounded-2xl w-full m-4">
-          <div className="">
+          <div className="w-full ">
+
             <h2 className="text-3xl text-black font-bold mb-8">Alta Docente</h2>
 
-            <form onSubmit={e => post(e)} className="grid grid-cols-2 gap-4 ">
-              <div id="contenedor1" className="flex flex-col m-2 ">
+            <form onSubmit={e => post(e)} className="flex flex-row flex-wrap justify-around w-full   ">
+              <div id="contenedor1" className="flex flex-col grow-2 w-1/3 m-2 text-center ">
+
+
                 <label className="label">
                   <span className="label-text  text-black">Tipo De Doc</span>
 
@@ -73,9 +84,11 @@ export default function AltaDocente() {
 
 
                 <label className="label">
-                  <span className="label-text text-black">Documento</span>
+                  <span className="label-text text-black">Documento
+                    <span className="label-text text-xs  text-black ml-44">(Sin puntos)</span>
+                  </span>
 
-                  <span className="label-text text-xs  text-black mr-20">(Sin puntos)</span>
+
 
                 </label>
                 <input
@@ -84,8 +97,8 @@ export default function AltaDocente() {
                   placeholder="Ingrese su Numero de Documento"
                   className="input rounded-full w-full text-black max-w-xs bg-white border-black"
                 />
-                <div className="content-center m-2 ml-5 pl-36">
-                  <button className="btn btn-sm  rounded-full bg-blue-600 text-white">
+                <div className="content-center m-2 ml-36">
+                  <button type="reset" className="btn btn-sm border-none rounded-full bg-blue-600 text-white">
                     Buscar
                   </button>
                 </div>
@@ -114,8 +127,9 @@ export default function AltaDocente() {
                   className="input rounded-full text-black w-full max-w-xs bg-white border-black"
                 />
                 <label className="label">
-                  <span className="label-text text-black">Crea tu contraseña</span>
-                  <span className="label-text text-xs  text-black pr-36">(Minimo 8 caracteres)</span>
+                  <span className="label-text text-black">Crea tu contraseña
+                    <span className="label-text text-xs text-black ml-20 ">(Minimo 8 caracteres)</span>
+                  </span>
                 </label>
                 <input
                   id="contra"
@@ -123,22 +137,38 @@ export default function AltaDocente() {
                   placeholder="Ingrese una Contraseña"
                   className="input rounded-full w-full text-black max-w-xs bg-white border-black"
                 />
+
+
+                <label className="label">
+                  <span className="label-text text-black">Confirmar  contraseña
+                    <span className="label-text text-xs text-black ml-20 ">(Minimo 8 caracteres)</span>
+                  </span>
+                </label>
+                <input
+                  id="contra2"
+                  type="password"
+                  placeholder="Repetir Contraseña"
+                  className="input rounded-full w-full text-black max-w-xs bg-white border-black"
+                />
+
+                <label className="label">
+                  <span className="label-text text-black ml-12">Localidad</span>
+                </label>
+                <input
+                  id="local"
+                  type="text"
+                  placeholder="Localidad"
+                  className="input rounded-full w-full text-black max-w-xs bg-white border-black"
+                />
+
               </div>
 
-              <div id="contenedor2" className="pl-10 ">
+              <div id="contenedor2" className=" flex flex-col grow-2 w-1/3 m-2 text-center">
                 <div className=" ">
-                  <label className="label">
-                    <span className="label-text text-black">Localidad</span>
-                  </label>
-                  <input
-                    id="local"
-                    type="text"
-                    placeholder="Localidad"
-                    className="input rounded-full w-full text-black max-w-xs bg-white border-black"
-                  />
+
 
                   <label className="label">
-                    <span className="label-text text-black">Direccion</span>
+                    <span className="label-text text-black ml-12">Direccion</span>
                   </label>
                   <input
                     id="direc"
@@ -148,18 +178,46 @@ export default function AltaDocente() {
                   />
 
                   <label className="label">
-                    <span className="label-text  text-black">Telefono</span>
-                    <span className="label-text text-xs  text-black">(Sin guines ni puntos)</span>
+                    <span className="label-text  text-black ml-12">Codigo de area </span>
+
+                  </label>
+                  <input
+                    id="codi1"
+                    type="number"
+                    placeholder="Ej:221"
+                    className="input rounded-full text-black mr-60 max-w-xs w-20 bg-white border-black"
+
+                  />
+
+                  <label className="label">
+                    <span className="label-text  text-black ml-12">Telefono
+                      <span className="label-text text-xs  text-black ml-36 ">(Sin guines ni puntos)</span>
+                    </span>
                   </label>
                   <input
                     id="tele"
                     type="number" name="numero"
                     placeholder="Numero de Telefono"
+                    maxLength="9"
                     className="input rounded-full text-black w-full max-w-xs bg-white border-black"
                   />
+
                   <label className="label">
-                    <span className="label-text  text-black">Tel Extra</span>
-                    <span className="label-text text-xs  text-black">(Sin guines ni puntos)</span>
+                    <span className="label-text  text-black ml-12">Codigo de area </span>
+
+                  </label>
+                  <input
+                    id="codi2"
+                    type="number"
+                    placeholder="Ej:221"
+                    className="input rounded-full text-black w-20 mr-60 max-w-xs bg-white border-black"
+
+                  />
+
+                  <label className="label">
+                    <span className="label-text  text-black ml-12">Tel Extra
+                      <span className="label-text text-xs  text-black ml-36 "  >(Sin guines ni puntos)</span>
+                    </span>
 
                   </label>
                   <input
@@ -167,21 +225,12 @@ export default function AltaDocente() {
                     type="number"
                     placeholder="Numero Extra"
                     className="input rounded-full text-black w-full max-w-xs bg-white border-black"
+
                   />
 
-                  <label className="label">
-                    <span className="label-text  text-black">Rol</span>
-                  </label>
-                  <select id="rol" defaultValue={"Tipo de Rol"} className="select rounded-full bg-white text-black border border-black select-ghost w-full max-w-xs">
-                    <option  >Tipo de Rol</option>
-                    <option>Admin</option>
-                    <option>Docente</option>
-
-                  </select>
-
 
                   <label className="label">
-                    <span className="label-text  text-black">Email</span>
+                    <span className="label-text  text-black ml-12">Email</span>
                   </label>
                   <input
                     id="correo"
@@ -189,15 +238,28 @@ export default function AltaDocente() {
                     placeholder="Ingrese su Email"
                     className="input rounded-full text-black w-full max-w-xs bg-white border-black"
                   />
+
+                  <label className="label">
+                    <span className="label-text  text-black ml-12">Rol</span>
+                  </label>
+                  <select id="rol" defaultValue={"Tipo de Rol"} className="select rounded-full bg-white text-black border border-black select-ghost w-full max-w-xs">
+                    <option  >Tipo de Rol</option>
+                    <option>Admin</option>
+                    <option>Docente</option>
+
+                  </select>
                 </div>
               </div>
-              <div className="content-center m-2 ">
-                <button onClick={(e) => limpiarFormulario(e)} type="reset" className="btn bg-blue-600 rounded-full text-white">Cancelar</button>
-              </div>
-              <div className="content-center m-2">
-                <button className="btn bg-blue-600 rounded-full text-white">
-                  Aceptar
-                </button>
+              <div className=" flex flex-row grow-3 w-full justify-evenly  m-2 text-center ">
+
+                <div className="content-center m-2 ">
+                  <button onClick={(e) => limpiarFormulario(e)} type="reset" className="btn bg-blue-600 rounded-full border-none text-white">Cancelar</button>
+                </div>
+                <div className="content-center m-2">
+                  <button type="submit" className="btn bg-blue-600 rounded-full border-none  text-white">
+                    Aceptar
+                  </button>
+                </div>
               </div>
             </form>
           </div>
--- src/services/AltaDocente.services.js
+++ src/services/AltaDocente.services.js
@@ -1,18 +1,22 @@
 export async function tryAltaDocente(data) {
-  const url = "http://192.168.33.31:8080/alta-docente"
-
-  if (data.tipo == "Tipo de Documento") { alert("seleccione un tipo de Documento") }
-  if (data.dni.length < 6) { alert("Dni incompleto") }
+  const url = "http://localhost:8080/alta-docente"
+  console.log("services")
+  console.log(data)
+/*   if (data.nro_legajo.length < 4) { alert("legajo invalido") }
+  if (data.tipo_dni == "Tipo de Documento") { alert("seleccione un tipo de Documento") }
+  if (data.nro_dni.length < 7) { alert("Dni incompleto") }
   if (data.nombre.length < 4) { alert("nombre invalido") }
   if (data.apellido.length < 4) { alert("apellido invalido") }
-  if (data.contrasena.length < 8) { alert("contraseña minimo 8 caracteres") }
+  if (data.password.length < 8) { alert("contraseña minimo 8 caracteres") }
   if (data.localidad.length < 1) { alert("complete el campo en localidad") }
   if (data.direccion.length < 1) { alert(" complete el campo en direccion") }
-  if (data.telefono.length < 10) { alert(" telefono incompleto") }
-  if (data.telExt.length < 10) { alert("telEtx incompleto") }
+  if (data.car_telefono.length < 3) { alert("complete el codigo de area en telefono") }
+  if (data.telefono.length < 7) { alert(" telefono incompleto") }
+  if (data.car_tel_extra.length < 3) { alert("complete el codigo de area en teleExt") }
+  if (data.telefono_extra.length < 7) { alert(" telEtx incompleto") }
   if (!data.email) { alert("complete el campo email") }
-  if (data.rol == "Tipo de Rol") { alert("seleccione un campo en rol") }
-
+  if (data.id_rol == "Tipo de Rol") { alert("seleccione un campo en rol") }
+ */
 
   // Default options are marked with *
   const response = await fetch(url, {
@@ -30,4 +34,5 @@ export async function tryAltaDocente(data) {
   });
   const res = await response.json(); // parses JSON response into native JavaScript objects
   console.log(res)
+  return res;
 }
