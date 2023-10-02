6c8b7bec78ea93df9406f88656709a58989d5fe5
Pc5404 <cfp5404@gmail.com>
2023-10-02 20:43:14 -0300

    modal funca

--- src/services/DatosDocentes.services.js
+++ src/services/DatosDocentes.services.js
@@ -1,31 +1,28 @@
-
-
 export async function getDataDocentes() {
-  const response = await fetch("http://localhost:8080/docentes")
+  const response = await fetch("http://localhost:8080/docentes");
   const docentes = await response.json();
-  console.log(docentes)
+  console.log(docentes);
   return docentes;
 }
 
-
-
 // Ejemplo implementando el metodo POST:
 export async function docenteModificado(data) {
-  const url = "http://192.168.33.31:8080/modificar-docente";
+  const url = "http://localhost:8080/docentes";
   // Opciones por defecto estan marcadas con un *
-  console.log(data)
+  console.log(data);
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
+  
   return response.json(); // parses JSON response into native JavaScript objects
 }
