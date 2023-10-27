5b878662e00708eb0c18e13decef77fca4391215
JikanSui <jikansui16@gmail.com>
2023-10-27 09:53:35 -0300

    asistencia component, render api data

--- src/components/AsistenciaAlumnos.jsx
+++ src/components/AsistenciaAlumnos.jsx
@@ -2,125 +2,73 @@ import { useQuery } from "@tanstack/react-query";
 import { getAsistencia, postJustificada } from "../services/AsistenciaAlumnos.services";
 
 function AsistenciaAlumnos() {
-   // const { data, isLoading, error } = useQuery(["getAsistencia"], getAsistencia)
-    const data = [
-/*         {
-            fecha:["1/12","3/12","5,12","9/12","11/12","13/12"],
-            curso: "Programación",
-            profesor:"Feltes Sebastian",
-            dias:"Lun, Mie, Vie",
-            horarios:"17.00 a 21.00",
-            cantAlumnos: 18,
-            alumnos:{
-                apellido:"Dias"
-            }
+	const { data, isLoading, error } = useQuery(["getAsistencia"], getAsistencia);
 
+	/* 	function justificarFalta(e) {
+		e.preventDefault();
+		let dni = e.target.id;
+		postJustificada(dni);
+	} */
 
-        } */
-    ]
-    function justificarFalta(e) {
-        e.preventDefault();
-        let dni = e.target.id;
-        postJustificada(dni)
-    }
+	if (!data) {
+		return <div>Cargando...</div>;
+	}
 
-    return (
+	const uniqueDates = [...new Set(data.map((item) => item.fecha))];
+	const uniqueStudents = [...new Map(data.map((item) => [item.id_alumno, item])).values()];
 
-
-        <div className="overflow-x-auto">
-
-            <div className="hero   ">
-                <div className="hero-content  p-2 bg">
-                    <h1 className="  p-3  font-normal  border-b border-blue-600 rounded-lg ">
-                        Nombre del curso:
-                        <span>
-                            {data ? data.curso : false}
-                        </span>
-                    </h1>
-                    <h2 className=" p-3 font-normal  border-b border-blue-600 rounded-lg">
-                        Nombre del profesor:
-                        <span>
-                            {data ? data.profesor : false}
-                        </span>
-                    </h2>
-                    <h2 className=" border-solid p-3 font-normal  border-b border-blue-600 rounded-lg  ">
-                        <span>
-                            Dias:
-                            {data ? data.dias : false}
-                        </span>
-                    </h2>
-                    <h2 className=" border-solid p-3 font-normal  border-b border-blue-600 rounded-lg ">
-                        Horarios:
-                        <span>
-                            {data ? data.horarios : false}
-                        </span>
-                    </h2>
-                    <h2 className=" p-3 font-normal   border-b border-blue-600 rounded-lg">
-                        Cantidad de alumnos:
-                        <span>
-                            {data ? data.cantAlumnos : false}
-                        </span>
-                    </h2>
-
-                </div>
-            </div>
-            <table className="table   ">
-
-                {/* head */}
-
-                <thead className="  ">
-                    <tr className="">
-                        <th >
-                            <label ></label>
-
-                        </th>
-
-                        <th className=" w-0  "><strong>Apellidos Alumno</strong></th>
-                        <th className=" w-0   "> <strong>Nombres Alumnno</strong></th>
-                        {data ? data.fecha.map(e => (
-                            <td>{e}</td>
-                        )) : false}
-                        <th>TOTALES</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* row 1 */}
-                    {data ?
-                        data.alumnos.map(el => (
-                            <tr className="hover:bg-slate-200">
-                                <td>
-
-                                </td>
-                                <td>
-                                    {el.apellido}
-                                </td>
-                                <td>
-                                    {el.nombre}
-                                </td>
-
-                                {
-                                    el.registros.map(e => (
-                                        <td className={e == "P" ? "text-blue-600 font-bold" : e == "A" ? "text-red-600 font-bold" : e == "1/2" ? "text-green-600 font-bold" : e == "J" ? "text-orange-600 font-bold" : false} >
-                                            {e != "A" ? e : <select onChange={(e) => justificarFalta(e)} id={el.dni} className="bg-transparent"><option value="A">A</option><option value="J">J</option></select>}
-                                        </td>
-                                    ))
-                                }
-                            </tr>
-                        ))
-                        : false}
-
-
-                </tbody>
-
-
-
-            </table>
-        </div>
-
-
-
-    )
+	return (
+		<div className="hero min-h-screen w-full bg-white">
+			<div className="hero-content h-full text-center border border-yellow-400 items-start">
+				<div className="w-full">
+					<div className="flex flex-row w-full justify-between text-black">
+						<h1 className="  p-3  font-normal  border-b border-blue-600 rounded-lg ">Nombre del curso:</h1>
+						<h2 className=" p-3 font-normal  border-b border-blue-600 rounded-lg">Nombre del profesor:</h2>
+						<h2 className=" border-solid p-3 font-normal  border-b border-blue-600 rounded-lg  ">
+							<span>Dias:</span>
+						</h2>
+						<h2 className=" border-solid p-3 font-normal  border-b border-blue-600 rounded-lg ">
+							Horarios:
+						</h2>
+						<h2 className=" p-3 font-normal   border-b border-blue-600 rounded-lg">Cantidad de alumnos:</h2>
+					</div>
+					<div className="overflow-x-scroll">
+						<table>
+							<thead>
+								<tr>
+									<th>Nombre</th>
+									<th>Apellido</th>
+									{uniqueDates.map((date, index) => (
+										<th key={index}>{date}</th>
+									))}
+								</tr>
+							</thead>
+							<tbody>
+								{uniqueStudents.map((student) => (
+									<tr key={student.id_alumno}>
+										<td>{student.nombre}</td>
+										<td>{student.apellido}</td>
+										{uniqueDates.map((date, index) => {
+											const attendanceRecord = data.find(
+												(item) => item.fecha === date && item.id_alumno === student.id_alumno
+											);
+											return (
+												<td key={index}>
+													{attendanceRecord
+														? attendanceRecord.descripcion.split("")[0].toUpperCase()
+														: "N/A"}
+												</td>
+											);
+										})}
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AsistenciaAlumnos;
@@ -131,3 +79,52 @@ export default AsistenciaAlumnos;
 “J“ (naranja): justificada
 
 “P“ (azul): presente. */
+
+{
+	/* <div className="overflow-x-auto">
+			<div className="hero ">
+				<div className="hero-content  p-2">
+					<h1 className="  p-3  font-normal  border-b border-blue-600 rounded-lg ">Nombre del curso:</h1>
+					<h2 className=" p-3 font-normal  border-b border-blue-600 rounded-lg">Nombre del profesor:</h2>
+					<h2 className=" border-solid p-3 font-normal  border-b border-blue-600 rounded-lg  ">
+						<span>Dias:</span>
+					</h2>
+					<h2 className=" border-solid p-3 font-normal  border-b border-blue-600 rounded-lg ">Horarios:</h2>
+					<h2 className=" p-3 font-normal   border-b border-blue-600 rounded-lg">Cantidad de alumnos:</h2>
+				</div>
+				<div>
+					<table>
+						<thead>
+							<tr>
+								<th>Nombre</th>
+								<th>Apellido</th>
+								{uniqueDates.map((date, index) => (
+									<th key={index}>{date}</th>
+								))}
+							</tr>
+						</thead>
+						<tbody>
+							{uniqueStudents.map((student) => (
+								<tr key={student.id_alumno}>
+									<td>{student.nombre}</td>
+									<td>{student.apellido}</td>
+									{uniqueDates.map((date, index) => {
+										const attendanceRecord = data.find(
+											(item) => item.fecha === date && item.id_alumno === student.id_alumno
+										);
+										return (
+											<td key={index}>
+												{attendanceRecord
+													? attendanceRecord.descripcion.split("")[0].toUpperCase()
+													: "N/A"}
+											</td>
+										);
+									})}
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div> */
+}
--- src/services/AsistenciaAlumnos.services.js
+++ src/services/AsistenciaAlumnos.services.js
@@ -1,33 +1,33 @@
+const url = "http://localhost:8080";
 
 export async function getAsistencia() {
-    const url = "http://192.168.33.31:8080/asistencia-alumnos"
-    const response = await fetch("http://192.168.33.31:8080/asistencia-alumnos");
-    const asistAlumn = await response.json();
-    console.log(asistAlumn)
-    return asistAlumn;
+	//const url = "http://192.168.33.31:8080/asistencia-alumnos"
+	const response = await fetch(`${url}/asistencia-alumnos/2`);
+	const asistAlumn = await response.json();
+	console.log(asistAlumn);
+	return asistAlumn;
 }
 
 export async function postJustificada(dni) {
-    const url = "http://192.168.33.31:8080/justificar-asistencia"
-    console.log(dni)
-    const data = {
-        dni:dni
-    }
-    const response = await fetch(url, {
+	const url = "http://192.168.33.31:8080/justificar-asistencia";
+	console.log(dni);
+	const data = {
+		dni: dni,
+	};
+	const response = await fetch(url, {
+		method: "POST", // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: "same-origin", // include, *same-origin, omit
+		headers: {
+			"Content-Type": "application/json",
+			// 'Content-Type': 'application/json',
+		},
+		redirect: "follow", // manual, *follow, error
+		referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		body: JSON.stringify(data), // body data type must match "Content-Type" header
+	});
 
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/json',
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-
-    const res = await response.json(); // parses JSON response into native JavaScript objects
-    return res;
+	const res = await response.json(); // parses JSON response into native JavaScript objects
+	return res;
 }
