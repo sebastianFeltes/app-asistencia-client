00ea3b7ccddbe77d34f7f4aeb752f9666914f88c
sebastianFeltes <jikansui16@gmail.com>
2023-11-17 20:35:45 -0300

    merge ocre

--- src/context/user.context.js
+++ src/context/user.context.js
@@ -1,8 +1,2 @@
 import { createContext } from "react";
 
-export const UserContext = createContext({
-  idUsuario: 1,
-  nombreUsuario: "Sebastian",
-  apellidoUsuario: "Feltes",
-  rolUsuario: 2,
-});
